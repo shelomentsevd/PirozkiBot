f6188f909f4e972a1b9e742ba900f275b2d96c21
Shelomentsev D <sedecim13@gmail.com>
2015-12-16 00:41:01 +0300

    Дописал загрузку всех пирожков из паблика. Нужно ещё сделать скрипт на подгрузку новых в бд, автоматический запуск скрипта каждые несколько часов и можно приниматься за написание бота.

--- cakes_thief.py
+++ cakes_thief.py
@@ -1,3 +1,4 @@
+import sys
 import pg8000
 import requests
 import json
@@ -5,7 +6,7 @@ import re
 from datetime import datetime
 
 def insertToDB(cursor, text, likes, reposts, date):
-	cursor.execute('INSERT INTO cakes(likes, reposts, text, date) values(%s, %s, "%s", "%s")' % (likes, reposts, text, date))
+	cursor.execute('INSERT INTO cakes(likes, reposts, text, date) values(%s, %s, %s, %s)', (likes, reposts, text, date))
 
 def iso8601(utc):
 	return datetime.fromtimestamp(utc).isoformat()
@@ -27,17 +28,32 @@ def author(raw_text):
 	result = re.sub('\[id[0-9^\|]+\||[\]]', '', raw_text).replace('&amp;', '&').strip()
 	return re.sub(' +', ' ', result)
 
-def main():
-	result = getwall('-28122932', 10, 1)
-	print json.dumps(result, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ':'))
-	for item in result[1:]:
+def insertAll(cursor, posts):
+	for item in posts[1:]:
 		text = br(item['text'])
 		raw_author = ''
 		likes   = item['likes']['count']
 		reposts = item['reposts']['count']
-		if(u'\u00A9' in text):
-			text, raw_author = text.split(u'\u00A9')
-			text = '%s--\n%s' % (text, author(raw_author))
-		print 'likes %s reposts %s' % (likes, reposts)
-		print text
+		text = author(text)
+		date = iso8601(item['date'])
+		insertToDB(cursor, text, likes, reposts, date)
+		#print 'likes %s reposts %s' % (likes, reposts)
+		#print '\n'+text
+
+def main():
+	db = pg8000.connect(user='cakesbot', password='cakesbot', database='cakesbot', host='localhost')
+	step = 100	
+	offset = 0
+	count = 0
+	cursor = db.cursor()
+	while True:
+		result = getwall('-28122932', step, offset)
+		print 'offset: %d count: %d' % (offset, count)
+		insertAll(cursor, result)
+		db.commit()
+		count = result[0]
+		if offset > count:
+			break
+		offset += step
+		
 main()
